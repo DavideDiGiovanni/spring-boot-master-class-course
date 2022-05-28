1624783d0740b27d52643f85a361ef16b7618899
DavideDiGiovanni <davidedigio@yahoo.it>
2022-05-28 17:15:26 +0200

    Switched from fake repository to the real one with a todo list.

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -12,6 +12,8 @@ public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         // TODO connect to a real DB
-        return Collections.emptyList();
+        return Collections.singletonList(
+                new Customer(1L, "TODO implements real db")
+        );
     }
 }
