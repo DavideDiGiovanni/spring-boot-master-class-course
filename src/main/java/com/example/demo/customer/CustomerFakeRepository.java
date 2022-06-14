41ef56c7a9df03728033dffcde01011bdb41f6f5
DavideDiGiovanni <davidedigio@yahoo.it>
2022-06-14 09:21:17 +0200

    Updated specific annotation for Repository and Service.

--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.customer;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component(value = "fake")
+@Repository(value = "fake")
 public class CustomerFakeRepository implements CustomerRepo {
 
     @Override
