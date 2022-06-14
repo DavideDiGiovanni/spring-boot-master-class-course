1f9a9a6dc149cdf537d95d427348ebd3aa16911b
DavideDiGiovanni <davidedigio@yahoo.it>
2022-06-14 12:13:31 +0200

    Added email field and validate annotation. Started to handle exception.

--- src/main/java/com/example/demo/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -7,8 +7,8 @@ public class CustomerFakeRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         return List.of(
-                new Customer(1L, "Dave Jones", "password123"),
-                new Customer(2L, "James Bond", "123password")
+                new Customer(1L, "Dave Jones", "password123", "email@gmail.com"),
+                new Customer(2L, "James Bond", "123password", "email@gmail.com")
                 );
     }
 }
